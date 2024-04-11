ba74e40f062a457b20cebefd324eac8c860ac416
pmr28 <pmr28@pitt.edu>
2024-04-11 15:36:00 -0400

    headfull

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -40,9 +40,6 @@ public class D3Test {
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--headless");
-    driver = new ChromeDriver(options);
   }
   @After
   public void tearDown() {
